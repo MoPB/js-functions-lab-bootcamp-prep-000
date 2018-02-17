05c6b59b6da47c39c5a71fb68091a67db0d4eb8c
Bertha Powell <mopowellb@gmail.com>
2018-02-17 15:09:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // write your code below!
+
+function happyHolidays() {
+  return("happyHolidays!")
+}
+ sayhappyHolidays()
+ 
+ 
+
+  
+  function happyHolidaysTo(Bertha) {
+    return('happyHolidays,${Bertha}!')
+  }
+  sayhappyHolidays('Bertha')
+  
+  
+function happyIndependenceDayTo(IndependenceDay,Bertha) {
+  console.log("Happy${IndependenceDay,${Bertha}!")
+}
+sayhappyIndependenceDayBertha()
+}
+  
+  function 'Mother'sdayCountdown('Mothers's Day, 20)' {
+    return "It\'s ${20} days until
+    ${Mother's Day}!"
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
