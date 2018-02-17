6a391c3951e38a296b65e76554063e2a70d377dc
Bertha Powell <mopowellb@gmail.com>
2018-02-17 13:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function happyHolidays() {
   
   
 function happyHolidayTo(Independence Day,Bertha) {
-  console.log('Happy Independence Day,${Bertha}!')
+  console.log("Happy$(IndependenceDay,${Bertha}!')
 }
 sayhappyIndependenceDay()
 
